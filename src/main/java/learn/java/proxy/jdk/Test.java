9b4f8b54bf9f6af452f8c8edc31521e4f7dfb01e
DREGON <1793686603@qq.com>
2020-11-30 20:45:20 +0800

    jdk动态代理

---
+++ src/main/java/learn/java/proxy/jdk/Test.java
@@ -0,0 +1,8 @@
+package learn.java.proxy.jdk;
+
+public class Test {
+    public static void main(String[] args) {
+        SmsService smsService = (SmsService) JdkProxyFactory.getProxy(new SmsServiceImpl());
+        smsService.send("java");
+    }
+}
